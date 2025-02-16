1662fb94a5e3e325cca53e1a32217af0ff1055a0
Karthikb2701@ <karthikb2701@gmail.com>
2025-02-16 21:07:00 +0530

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Sidebar } from "./components/sidebar/Sidebar";
+import { LoginUI } from "./components/loginUI/LoginUI";
 import SidebarContextProvider from "./components/sidebar/sidebar_context";
 
 function App() {
   return (
     <SidebarContextProvider>
       <Sidebar />
+      <LoginUI />
       <main className="">
         <div className="h-[200vh]" />
       </main>
